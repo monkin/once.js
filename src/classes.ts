ea35b9e5ee47062194285b64f493b94844315e63
Andrey Monkin <amonkin@evolutiongaming.com>
2017-09-11 13:29:13 +0300

    Classes function refactoring

--- src/classes.ts
+++ src/classes.ts
@@ -5,7 +5,7 @@ export namespace Internal {
     export type SimplePrimitiveValue = string | null | undefined | false;
     export interface SimpleArrayValue extends Array<SimplePrimitiveValue | SimpleArrayValue> {};
     export type SimpleValue = SimplePrimitiveValue | SimpleArrayValue;
-    export type FlagsValue = { [key: string]: () => boolean };
+    export type FlagsValue = { [key: string]: Param<boolean> };
     export interface ArrayValue extends Array<Param<SimpleValue> | FlagsValue | ArrayValue> {};
 
     export type ClassValue = Param<SimpleValue> | FlagsValue | ArrayValue;
@@ -30,15 +30,37 @@ export namespace ClassValue {
             } else if (value instanceof Function) {
                 return () => stringify(value()) as string;
             } else {
-                return () => {
+                if (Object.keys(value).some(k => Param.isFunction(value[k]))) {
+                    let prefix = "",
+                        methods = [] as (() => string)[];
+                    for (let i in value) {
+                        let v = value[i];
+                        if (Param.isValue(v)) {
+                            if (v) {
+                                prefix = concat(prefix, i);
+                            }
+                        } else {
+                            ((i: string, v: () => boolean) => {
+                                methods.push(() => v() ? i : "");
+                            })(i, v);
+                        }
+                    }
+                    return () => {
+                        let r = prefix;
+                        for (let m of methods) {
+                            r = concat(r, m());
+                        }
+                        return r;
+                    };
+                } else {
                     let r = "";
                     for (let i in value) {
-                        if (value[i]()) {
+                        if (value[i]) {
                             r = concat(r, i);
                         }
                     }
                     return r;
-                };
+                }
             }
         } else {
             return "";
